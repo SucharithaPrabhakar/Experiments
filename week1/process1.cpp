f919eeac85e5ea012b4dda3ae955b6ce2f3f157b
sucharitha <sprabhakar@ucla.edu>
2016-10-20 07:07:42 -0700

    8 threads

--- week1/process1.cpp
+++ week1/process1.cpp
@@ -6,20 +6,20 @@
 #include <mutex>
 #include <thread>
 #include <vector>
-#define NUM_THREADS 2
+#define NUM_THREADS 8
 
 int main(int argc, char* argv[ ]) 
 {
     std::vector<std::thread> threads(NUM_THREADS);
-    std::mutex io_mutex;
+    std::array<std::mutex, 4> io_mutex;
     std::string msg_q;
     using namespace boost::interprocess;
-    long int execCount[] = {0, 0};
-    std::string mqName = "queue1";
+    long int execCount[] = {0, 0, 0, 0, 0, 0, 0, 0};
+    std::string mqName[] = {"queue1", "queue2", "queue3", "queue4"};
     int buffer;
     size_t recvd_size;
     unsigned int priority;
-    time_t end = time(NULL) + 240;
+    time_t end = time(NULL) + 68;
     for (unsigned int i = 0; i < NUM_THREADS; i++)
     {
       thread_local int msg_num = 0;
@@ -29,11 +29,11 @@ int main(int argc, char* argv[ ])
         {
           try 
           {
-                if((i%2) == 1) //Thread 1 in this case, reader 
-                { 
-                  message_queue mq (open_only, mqName.c_str());
+             switch(i) { 
+                case 0: {
+                  message_queue mq (open_or_create, mqName[0].c_str(), 1, sizeof(int));
                   { 
-                    std::lock_guard<std::mutex> iolock(io_mutex);
+                    std::lock_guard<std::mutex> iolock(io_mutex[0]);
                     if((mq.get_num_msg() == 1) && mq.try_receive((void*) &buffer, sizeof(int), recvd_size, priority))
                     {
                       //Increment execCount only on successful send just to keep track of dependency on receiver.
@@ -43,11 +43,11 @@ int main(int argc, char* argv[ ])
                     }
                   }
                 }
-                else if((i%2) == 0) //Thread 0 in this case, writer
-                {
-                   message_queue mq (open_or_create, mqName.c_str(),  1, sizeof(int));
+                break;
+                case 1: {
+                   message_queue mq (open_or_create, mqName[0].c_str(),  1, sizeof(int));
                    {
-                      std::lock_guard<std::mutex> iolock(io_mutex);
+                      std::lock_guard<std::mutex> iolock(io_mutex[0]);
                       if((mq.get_num_msg() == 0) && (mq.try_send(&msg_num, sizeof(int), 0)))
                       {
                         //Increment execCount only on successful send just to keep track of dependency on receiver.
@@ -57,21 +57,107 @@ int main(int argc, char* argv[ ])
                       }
                    }
                 }
-            } 
-            catch (interprocess_exception& e) 
-            { 
-              std::cout << e.what( ) << std::endl;
+                break;
+                case 2: {
+                  message_queue mq (open_or_create, mqName[1].c_str(), 1, sizeof(int));
+                  { 
+                    std::lock_guard<std::mutex> iolock(io_mutex[1]);
+                    if((mq.get_num_msg() == 1) && mq.try_receive((void*) &buffer, sizeof(int), recvd_size, priority))
+                    {
+                      //Increment execCount only on successful send just to keep track of dependency on receiver.
+                      execCount[i]++;
+                      msg_num++;
+                      std::cout << "Thread id\t" << syscall( __NR_gettid ) <<  "\tCPU\t" << sched_getcpu() << "\tExec Count\t" << execCount[i] << "\tReceived\t" << buffer << "\tMessage number\t" << msg_num << std::endl;
+                    }
+                  }
+                 }
+                 break;
+                 case 3: {
+                   message_queue mq (open_or_create, mqName[1].c_str(),  1, sizeof(int));
+                   {
+                      std::lock_guard<std::mutex> iolock(io_mutex[1]);
+                      if((mq.get_num_msg() == 0) && (mq.try_send(&msg_num, sizeof(int), 0)))
+                      {
+                        //Increment execCount only on successful send just to keep track of dependency on receiver.
+                        execCount[i]++;
+                        msg_num++;
+                        std::cout << "Thread id\t" << syscall( __NR_gettid ) <<  "\tCPU\t" << sched_getcpu() << "\tExec Count\t" << execCount[i] << "\tMessage Number\t" << msg_num << std::endl;
+                      }
+                   }
+                }
+                break;
+                case 4: {
+                  message_queue mq (open_or_create, mqName[2].c_str(), 1, sizeof(int));
+                  {
+                    std::lock_guard<std::mutex> iolock(io_mutex[2]);
+                    if((mq.get_num_msg() == 1) && mq.try_receive((void*) &buffer, sizeof(int), recvd_size, priority))
+                    {
+                      //Increment execCount only on successful send just to keep track of dependency on receiver.
+                      execCount[i]++;
+                      msg_num++;
+                      std::cout << "Thread id\t" << syscall( __NR_gettid ) <<  "\tCPU\t" << sched_getcpu() << "\tExec Count\t" << execCount[i] << "\tReceived\t" << buffer << "\tMessage number\t" << msg_num << std::endl;
+                    }
+                  }
+                 }
+                 break;
+                 case 5: {
+                   message_queue mq (open_or_create, mqName[2].c_str(),  1, sizeof(int));
+                   {
+                      std::lock_guard<std::mutex> iolock(io_mutex[2]);
+                      if((mq.get_num_msg() == 0) && (mq.try_send(&msg_num, sizeof(int), 0)))
+                      {
+                        //Increment execCount only on successful send just to keep track of dependency on receiver.
+                        execCount[i]++;
+                        msg_num++;
+                        std::cout << "Thread id\t" << syscall( __NR_gettid ) <<  "\tCPU\t" << sched_getcpu() << "\tExec Count\t" << execCount[i] << "\tMessage Number\t" << msg_num << std::endl;
+                      }
+                   }
+                }
+                break;
+                case 6: {
+                  message_queue mq (open_or_create, mqName[3].c_str(), 1, sizeof(int));
+                  {
+                    std::lock_guard<std::mutex> iolock(io_mutex[3]);
+                    if((mq.get_num_msg() == 1) && mq.try_receive((void*) &buffer, sizeof(int), recvd_size, priority))
+                    {
+                      //Increment execCount only on successful send just to keep track of dependency on receiver.
+                      execCount[i]++;
+                      msg_num++;
+                      std::cout << "Thread id\t" << syscall( __NR_gettid ) <<  "\tCPU\t" << sched_getcpu() << "\tExec Count\t" << execCount[i] << "\tReceived\t" << buffer << "\tMessage number\t" << msg_num << std::endl;
+                    }
+                  }
+                 }
+                 break;
+                 case 7: {
+                   message_queue mq (open_or_create, mqName[3].c_str(),  1, sizeof(int));
+                   {
+                      std::lock_guard<std::mutex> iolock(io_mutex[3]);
+                      if((mq.get_num_msg() == 0) && (mq.try_send(&msg_num, sizeof(int), 0)))
+                      {
+                        //Increment execCount only on successful send just to keep track of dependency on receiver.
+                        execCount[i]++;
+                        msg_num++;
+                        std::cout << "Thread id\t" << syscall( __NR_gettid ) <<  "\tCPU\t" << sched_getcpu() << "\tExec Count\t" << execCount[i] << "\tMessage Number\t" << msg_num << std::endl;
+                      }
+                   }
+                }
+                break;
             }
          } 
-      });
-    }
+         catch (interprocess_exception& e) 
+         { 
+           std::cout << e.what( ) << std::endl;
+         }
+      } 
+    });
+  }
 
-    for (auto& t : threads) {
+  for (auto& t : threads) {
       t.join();
-    }
-    for(unsigned int i = 0; i < NUM_THREADS; i++)
-    {
-      std::cout << "Exec count of thread\t" << i << "is\t" << execCount[i] << std::endl;
-    }
-    return 0;
+  }
+  for(unsigned int i = 0; i < NUM_THREADS; i++)
+  {
+    std::cout << "Exec count of thread\t" << i << "is\t" << execCount[i] << std::endl;
+  }
+  return 0;
 }
